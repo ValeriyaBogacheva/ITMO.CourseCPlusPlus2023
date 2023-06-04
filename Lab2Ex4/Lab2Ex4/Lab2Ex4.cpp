194a6a7aa529db18a9ac4be14478f715044cbc37
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-04 11:56:09 +0300

    Add files via upload

---
+++ Lab2Ex4/Lab2Ex4/Lab2Ex4.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	int s, k, m;
+	s = 0;
+	cout << "Введите диапазон k и m =>";
+	cin >> k >> m;
+	for (int i = 1; i <= 100; i++)
+	{
+		if ((i > k) && (i < m))
+			continue;
+		s += i;
+	}
+	cout << "Сумма = " << s;
+	return 0;
+}
+
