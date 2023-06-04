3bebce13db9965c5f9725b8be11a5d03461285bc
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-04 11:53:14 +0300

    Add files via upload

---
+++ Lab2Ex2/Lab2Ex2_2/Lab2Ex2_2/Lab2Ex2_2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	int a, b, temp;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	while (a != b)
+	{
+		if (a > b)
+			a -= b; 
+		else
+			b -= a;
+	}
+	cout << "НОД = " << a << endl;
+
+}
+
