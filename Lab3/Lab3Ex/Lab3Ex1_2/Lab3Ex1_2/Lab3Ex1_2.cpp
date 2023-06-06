1e96af16122d1a9f54088c03bf4e58ca4f232291
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-06 21:04:49 +0300

    Add files via upload

---
+++ Lab3/Lab3Ex/Lab3Ex1_2/Lab3Ex1_2/Lab3Ex1_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string privet(string name)
+{
+	string str = name + ", " + "hello!\n";
+	return str;
+}
+int main()
+{
+	string name;
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout<<privet(name);
+	return 0;
+}
