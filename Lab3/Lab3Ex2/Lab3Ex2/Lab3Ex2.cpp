43c8f6244479eb43aa54a92456297edc4165b34a
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-06 21:11:09 +0300

    Add files via upload

---
+++ Lab3/Lab3Ex2/Lab3Ex2/Lab3Ex2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string privet(string name)
+{
+	string str = name + ", " + "hello!\n";
+	return str;
+}
+void privet(string name, int k)
+{
+	cout << name << ", " << "hello! " << "you input " << k << endl;
+}
+
+int main()
+{
+	string name;
+	int k;
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout << "Input number:" << endl;
+	cin >> k;
+	string str = privet(name);
+	privet(name, k);
+
+	return 0;
+}
---
+++ Lab3/Lab3Ex4/Lab3Ex4/Lab3Ex4.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+int addNumders(int n)
+{
+    if (n == 1) return 1; // выход из рекурсии
+    else return (n + addNumders(n - 1));
+}
+
+
+int addNumders(int a,int n)
+{
+    if (a == n) return n; // выход из рекурсии
+    else return (a + addNumders(a + 1,n));
+}
+int gcd(int m, int n)
+{
+    if (n == 0) return m;
+    return gcd(n, m % n);
+}
+int main()
+{
+    int a,n;
+    cout << "Input a, n => ";
+    cin >> a >> n;
+    cout << "addNmbers => " << addNumders(a, n)<<endl;
+    cout << "gcd => " << gcd(a, n);
+}
+
