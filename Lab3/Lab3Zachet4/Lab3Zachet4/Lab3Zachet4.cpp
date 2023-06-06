d3227a653c1a9ead18deeb18e8a9c467cc231f91
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-06 21:14:09 +0300

    Add files via upload

---
+++ Lab3/Lab3Zachet4/Lab3Zachet4/Lab3Zachet4.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int sum(int n) {
+    int s=0;
+    if (n == 1) return 5;
+    else return(n * 5 + sum(n - 1));
+}
+
+int main()
+{
+    int n;
+    system("chcp 1251");
+    cout << "Введите n =>";
+    cin >> n;
+    cout<< "Сумма ряда = "<< sum(n);
+
+}
+
---
+++ Lab3/Lab3Zachet5/Lab3Zachet5/Lab3Zachet5.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int Rec10to2(int number) {
+    if (number > 1)
+        Rec10to2(number / 2);
+    cout << number % 2;
+    return number;
+}
+
+int main()
+{
+    int number;
+    system("chcp 1251");
+    cout << "Введите натуральное число => ";
+    cin >> number;
+    Rec10to2(number);
+}
