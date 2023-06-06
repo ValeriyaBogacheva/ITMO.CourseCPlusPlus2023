81b420fb0dbde0c0874ca2b8b2f27a1c419f059f
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-06 20:39:53 +0300

    Add files via upload

---
+++ Lab2/Lab2Ex1/Lab2Ex1/Lab2Ex1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    system("chcp 1251");
+    cout << "Введите координаты x и y =>";
+    double x, y;
+    cin >> x >> y;
+    if (x * x + y * y < 9 && y > 0)
+        cout << "Внутри";
+    else if (x * x + y * y > 9 || y < 0)
+        cout << "Снаружи";
+    else cout << "На границе";
+
+}
+
