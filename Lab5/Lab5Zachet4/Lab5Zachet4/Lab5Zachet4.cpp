c3f93bd91660eb5f0c4a335cc07205e27541fda2
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-08 21:33:16 +0300

    Add files via upload

---
+++ Lab5/Lab5Zachet4/Lab5Zachet4/Lab5Zachet4.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char** argv) {
+    system("chcp 1251");
+    int i;
+
+    if (strncmp(argv[1], "-a", 2) == 0) {
+        int x = atoi(argv[2]);
+        int y = atoi(argv[3]);
+        cout << "Сумма: "<<x+y<<endl;
+    }else  if (strncmp(argv[1], "-m", 2) == 0) {
+        int x = atoi(argv[2]);
+        int y = atoi(argv[3]);
+        cout << "Произведение: " << x * y << endl;
+    }
+
+}
