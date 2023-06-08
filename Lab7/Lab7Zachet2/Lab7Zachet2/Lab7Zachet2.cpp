9c80fa94439b1bc29ec5998b9e9a6c159fa4cbee
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-08 21:52:27 +0300

    Add files via upload

---
+++ Lab7/Lab7Zachet2/Lab7Zachet2/Lab7Zachet2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+struct quadr {
+	double x1;
+	double x2;
+	bool status = false;
+};
+
+quadr Quadratic(int a,int b,int c) {
+	double d;
+	quadr q = quadr();
+	d = b * b - 4 * a * c;
+	if (d > 0) 
+	{
+		q.x1 = ((-b) + sqrt(d)) / (2 * a);
+		q.x2 = ((-b) - sqrt(d)) / (2 * a);
+	}
+	else if (d == 0) 
+	{
+		q.x1 = -(b / (2 * a));
+	}
+	else {
+		q.status = false;
+	}
+	return q;
+}
+int main()
+{
+	system("chcp 1251");
+
+	double a, b, c;
+	cout << "Введите коэф-ты уравнения=> ";
+	cin >> a >> b >> c;
+	quadr q = Quadratic(a, b, c);
+	if (!q.status) {
+		cout << "Корни:"<<endl <<" x1 : " << q.x1 <<endl<< " x2 : " << q.x2 << endl;
+	}
+	else {
+		cout << "Корней нет";
+	}
+}
