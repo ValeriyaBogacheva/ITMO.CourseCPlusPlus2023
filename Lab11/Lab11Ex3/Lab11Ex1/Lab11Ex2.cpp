2544a53e263afd474200bc7d3675165d60fc1991
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-10 10:59:22 +0300

    Add files via upload

---
+++ Lab11/Lab11Ex3/Lab11Ex1/Lab11Ex2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include "Distance.h"
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    Distance dist1 = 2.35F;
+    dist1.showdist();
+    float mtrs = static_cast<float>(dist1);
+
+    float d = 2.6 + dist1;
+    cout << mtrs<<"\t" << d<<endl;
+
+    float d2 = dist1 + (float)5.78;
+    cout << mtrs << "\t" << d2 << endl;
+
+    float d3 = 10.52 - dist1;
+    cout << mtrs << "\t" << d3 << endl;
+
+    float d4 = dist1 - (float)2.1;
+    cout << mtrs << "\t" << d4;
+
+}
+
