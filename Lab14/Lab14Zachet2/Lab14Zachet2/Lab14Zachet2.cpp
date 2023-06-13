95842156118a72818aa3f3dd4d9c7bf3337f6c8c
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-06-13 20:35:38 +0300

    Add files via upload

---
+++ Lab14/Lab14Zachet2/Lab14Zachet2/Lab14Zachet2.cpp
@@ -0,0 +1,27 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+template<typename T>
+void Print(const T& data, const string& del) {
+    for (auto iter = begin(data); iter != end(data); ++iter) {
+        cout << *iter;
+        if (next(iter) == end(data)) {
+            cout << endl;
+            return;
+        }
+        cout << del;
+    }
+    
+}
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    vector<int> data = { 1, 2, 3,4,5,6,7,8,9,10,15 };
+    Print(data, ", ");
+}
+
